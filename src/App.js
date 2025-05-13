d6399cceb0658ca352563091d5fb88d2069d4ba3
Akash Kumar Gaur <149656396+akashgaur-twidpay@users.noreply.github.com>
2025-05-13 12:46:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import { useEffect, useRef, useState } from 'react';
+import { bootstrapCameraKit } from '@snap/camera-kit';
 
-function App() {
+const App = () => {
+  const canvasRef = useRef(null);
+  const camKitRef = useRef(null);
+  const sessionRef = useRef(null);
+  const initialized = useRef(false);
+
+  useEffect(() => {
+    const init = async () => {
+      if (initialized.current) return; // prevent re-initialization
+      initialized.current = true;
+      const cameraKit = await bootstrapCameraKit({
+        apiToken: '',
+      });
+      const liveRenderTarget = document.getElementById('canvas');
+      const session = await cameraKit.createSession({ liveRenderTarget });
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      camKitRef.current = cameraKit;
+      sessionRef.current = session;
+      await session.setSource(mediaStream);
+      await session.play();
+      const lens = await camKitRef.current.lensRepository.loadLens("LENS_ID", "LENS_GROUP_ID");
+      await sessionRef.current.applyLens(lens);
+    };
+    init();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        textAlign: 'center',
+        padding: '2rem',
+        flexDirection: 'column',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <canvas
+        ref={canvasRef}
+        width='640'
+        height='480'
+        style={{ border: '1px solid black', width: 640, height: 480 }}
+        id='canvas'
+      />
     </div>
   );
-}
+};
 
 export default App;
